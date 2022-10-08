bfa5bde3ad5fda80fb4765dbd3db872cb53e1514
MartinChristopher <519483040@qq.com>
2022-10-08 14:56:38 +0800

    1.0.0【1.0.0.0】

---
+++ Example/Pods/Target Support Files/FCPrivacyAuthTool/FCPrivacyAuthTool-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FCPrivacyAuthTool : NSObject
+@end
+@implementation PodsDummy_FCPrivacyAuthTool
+@end
---
+++ Example/Pods/Target Support Files/Pods-FCPrivacyAuthTool_Example/Pods-FCPrivacyAuthTool_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FCPrivacyAuthTool_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FCPrivacyAuthTool_Example
+@end
